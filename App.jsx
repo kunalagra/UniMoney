c9a3be6c4172dce9a10a7975789f193444ec1a8f
Ganesh Utla <ganeshutla888@gmail.com>
2023-11-09 19:50:22 +0530

    Added image assets

--- App.jsx
+++ App.jsx
@@ -73,7 +73,7 @@ function App() {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Lessgoo!!! Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            Yay!!! Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
