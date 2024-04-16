12552830c13064f7433d0cab6815e9169990e0c2
Kunal Agrawal <66416646+kunalagra@users.noreply.github.com>
2024-04-16 19:49:04 +0530

    fix: Dynamic set AuthToken for Requests (Axios)

--- httpClient.js
+++ httpClient.js
@@ -1,15 +1,29 @@
 import axios from "axios";
-const url = "https://unimoney-backend.onrender.com/";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default axios.create({
+const url = "https://unimoney-backend.onrender.com/";
+
+const axiosInstance = axios.create({
+  baseURL: url,
   withCredentials: true,
   accessControlAllowCredentials: true,
-  credientials: "same-origin",
+  credentials: "same-origin",
   headers: {
     "Content-type": "application/json",
-    "Authorization": "Bearer " + await AsyncStorage.getItem('token')
   },
-  baseURL: url
 });
 
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
